9d0e9043aff1a6d20fc12bd5fea6abcd24aca132
Lalbek Sherbekov <sherbekovlalbek21@gmail.com>
2024-05-15 16:04:35 +0200

    Crypto-header is ready

--- src/context/crypto-context.jsx
+++ src/context/crypto-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { fakeFetchCrypto, fetchAssets } from "../components/api";
 import { percentDifference } from "../utilis";
 
@@ -13,33 +13,43 @@ export function CryptoContextProvider({ children }) {
   const [crypto, setCrypto] = useState([]);
   const [assets, setAssets] = useState([]);
 
+  function mapAssets(assets,result) {
+    return assets.map((asset) => {
+      const coin = result.find((c) => c.id === asset.id);
+    return {
+      ...asset,
+      grow: asset.price < coin.price,
+      growPercent: percentDifference(asset.price, coin.price),
+      totalAmount: asset.amount * coin.price,
+      totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+    };
+    })
+    
+  }
+
   useEffect(() => {
     async function preload() {
       setLoading(true);
       const { result } = await fakeFetchCrypto();
       const assets = await fetchAssets();
-      setAssets(
-        assets.map((asset) => {
-          const coin = result.find((c) => c.id === asset.id);
-          return {
-            ...asset,
-            grow: asset.price < coin.price,
-            growPercent: percentDifference(asset.price, coin.price),
-            totalAmount: asset.amount * coin.price,
-            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
-          };
-        })
-      );
-
+      setAssets( mapAssets(assets,result));
       setCrypto(result);
       setLoading(false);
     }
     preload();
   }, []);
+function addAsset(newAsset) {
+setAssets(prev=> mapAssets([...prev, newAsset], crypto ))
+}
+
   return (
-    <CryptoContext.Provider value={{ assets, crypto, loading }}>
+    <CryptoContext.Provider value={{ assets, crypto, loading, addAsset }}>
       {children}
     </CryptoContext.Provider>
   );
 }
 export default CryptoContext;
+
+export function useCrypto() {
+return useContext(CryptoContext)
+}
